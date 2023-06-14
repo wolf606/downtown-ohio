1860229e293649b2377603f06e2629280113d61d
wolf880 <luismsanchez808@gmail.com>
2023-06-13 21:36:50 -0500

    Fix bug where update.sh gets stucked updating Rust

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
 RUST_SERVER_PATH="$(dirname "$(pwd)")/Rust"
+
+# Check if path exists, if not create it
+
+if [ ! -d "$RUST_SERVER_PATH" ]; then
+    mkdir -p "$RUST_SERVER_PATH"
+fi
+
 steamcmd +force_install_dir $RUST_SERVER_PATH +login anonymous +app_update 258550 +quit
 ./oxide.sh

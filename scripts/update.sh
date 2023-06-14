bf16ef3a3ccb2a7299d06e6caf24438fc84a49ee
wolf880 <luismsanchez808@gmail.com>
2023-06-13 21:32:02 -0500

    Fix update.sh script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-
-steamcmd +force_install_dir /home/rusty/Steam/Rust +login anonymous +app_update 258550 +quit
+RUST_SERVER_PATH="$(dirname "$(pwd)")/Rust"
+steamcmd +force_install_dir $RUST_SERVER_PATH +login anonymous +app_update 258550 +quit
 ./oxide.sh
